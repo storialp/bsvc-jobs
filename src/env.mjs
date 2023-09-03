704597dc2f59edf45c0fc73a77bde7ce465506e2
storialp <pericchiluis@gmail.com>
2023-09-03 18:27:21 +0200

    Fixed clerk provider and environment validation

--- src/env.mjs
+++ src/env.mjs
@@ -1,4 +1,5 @@
 import { createEnv } from "@t3-oss/env-nextjs";
+import { url } from "inspector";
 import { z } from "zod";
 
 export const env = createEnv({
@@ -18,7 +19,7 @@ export const env = createEnv({
    * `NEXT_PUBLIC_`.
    */
   client: {
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
   },
 
   /**
@@ -28,7 +29,7 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
-    CLERK_SECRET_KEY: process.env.CLIENT_SECRET_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
       process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
   },
