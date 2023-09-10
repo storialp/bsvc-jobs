c0b74a0d1243b18f7b36e3718cae8d96b0011f57
storialp <pericchiluis@gmail.com>
2023-09-10 18:43:41 +0200

    Added job schema & routes, put job list in container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
+import { useUser } from "@clerk/nextjs";
 import Head from "next/head";
-import Link from "next/link";
 import JobsList from "~/components/JobList";
 import Navbar from "~/components/Navbar";
 
 import { api } from "~/utils/api";
 
 export default function Home() {
+  const user = useUser();
+
+  const { data } = api.job.getAll.useQuery();
   return (
     <>
       <Head>
@@ -16,7 +19,12 @@ export default function Home() {
         />
       </Head>
       <Navbar />
-      <JobsList />
+      <div className="px-6 font-medium">
+        <h1 className="ml-3 mt-5 text-2xl  text-gray-800">Jobs</h1>
+        <div className="mx-auto h-auto w-full rounded-lg border-2 px-3 py-3 shadow-md">
+          <JobsList />
+        </div>
+      </div>
     </>
   );
 }
--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { exampleRouter } from "~/server/api/routers/example";
+import { jobRouter } from "~/server/api/routers/job";
 import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -7,7 +7,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  job: jobRouter,
 });
 
 // export type definition of API
