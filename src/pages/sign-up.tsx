463feb52375792ba111f4e6fb1a56b93d879906a
storialp <96848889+storialp@users.noreply.github.com>
2023-10-02 16:02:43 +0200

    Cetnered sign-up element

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,5 +1,13 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <SignUp
+      appearance={{
+        elements: {
+          rootBox: "mx-auto pt-20",
+        },
+      }}
+    />
+  );
 }
