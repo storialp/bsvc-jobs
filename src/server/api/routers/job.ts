f77a880b019c2f922b4263606f80e94effa30560
storialp <96848889+storialp@users.noreply.github.com>
2023-10-03 11:04:40 +0200

    Added createJob EP

--- src/server/api/routers/job.ts
+++ src/server/api/routers/job.ts
@@ -75,4 +75,56 @@ export const jobRouter = createTRPCRouter({
         },
       });
     }),
+
+  createJob: privateProcedure
+    .input(
+      z.object({
+        title: z.string().min(4).max(75),
+        companyName: z.string().min(1).max(150),
+        city: z.string().min(1).max(30),
+        country: z.string().min(1).max(30),
+        function: z.string().min(1).max(75),
+        schedule: z.string().min(1).max(40),
+        imageUrl: z.string().min(1).max(200),
+        link: z.string().min(1).max(200),
+        descriptionContent: z.string().array().min(0).max(10),
+        qualificationContent: z.string().array().min(0).max(10),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const userId = ctx.userId;
+      if (userId !== "user_2Uu9aGWBSR23pFvjxqDObRHihhn")
+        return { unauthorized: true };
+      const descriptionArray: { content: string }[] = [];
+      const qualificationArray: { content: string }[] = [];
+      if (input.descriptionContent.length > 0) {
+        input.descriptionContent.forEach((description) => {
+          descriptionArray.push({ content: description });
+        });
+      }
+      if (input.qualificationContent.length > 0) {
+        input.descriptionContent.forEach((qualification) => {
+          qualificationArray.push({ content: qualification });
+        });
+      }
+
+      const job = {
+        title: input.title,
+        companyName: input.companyName,
+        city: input.city,
+        country: input.country,
+        function: input.function,
+        schedule: input.schedule,
+        imageUrl: input.imageUrl,
+        link: input.link,
+        description: {
+          create: descriptionArray,
+        },
+        qualification: {
+          create: qualificationArray,
+        },
+      };
+      await ctx.prisma.job.create({ data: job });
+      return { jobCreated: true };
+    }),
 });
